edce206267ed26f9e3c5ccb7f8f2bc1924c4a687
Chun Sun <sunchun79@gmail.com>
2017-02-01 22:08:58 -0500

    a working version with msft account login

---
+++ docomm/docomm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="docomm.MvcApplication" Language="C#" %>
---
+++ docomm/docomm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(docomm.Startup))]
+namespace docomm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
